2127ef1ac4c630006a2822a0c8ec369c5b891aa3
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-17 17:15:46 +0300

    Update View.composition.js

--- src/js/View.composition.js
+++ src/js/View.composition.js
@@ -1,20 +1,54 @@
 import cr from './helpers.js';
+import Model from './Model.composition.js';
+import Template from './Template.js';
 
-function View() {
+function View(template) {
 
     const app = document.getElementById('app');
 
     const form = cr('form', 'form-todo');
 
-    console.log(app);
-    
+    const input = cr('input', 'input-todo');
+    input.type = 'text';
 
+    const submit = cr('button', 'btn-todo');
+    submit.textContent = 'Добавить';
+
+    const todoList = cr('ul', 'ul-todo');
+
+    form.append(input, submit);
+    app.append(form, todoList);
 
     return {
-        bindAdd() {
+        show(items) {
+            console.log(items);
             
+            todoList.innerHTML = items.reduce((accumulator, item) => accumulator + `
+<li data-id="${item.id}"${item.completed ? ' class="completed"' : ''}>
+	<div class="view">
+		<input class="toggle" type="checkbox" ${item.completed ? 'checked' : ''}>
+		<label>${escapeForHTML(item.title)}</label>
+		<button class="destroy"></button>
+	</div>
+</li>`, '');
+        },
+        bindAdd(handler) {
+            form.addEventListener('submit', e => {
+                event.preventDefault();
+                if (input.value) {
+                    let a = handler(input.value);
+                    console.log(a);
+                    
+                    form.reset();
+                    this.show(handler(input.value));
+                }
+            });
         },
     }
 }
 
-const view = View();
+const template = Template();
+const model = Model();
+
+const view = View(template);
+view.bindAdd(model.add.bind(this));
