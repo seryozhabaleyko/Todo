48c599a59f1b348c9d91ec193a2146ce4d91b0f2
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-17 12:19:35 +0300

    Create Model.js

---
+++ src/js/Model.js
@@ -0,0 +1,50 @@
+class Model {
+    constructor() {
+        this.todos = JSON.parse(localStorage.getItem('todos')) || []
+    }
+
+    bindTodoListChanged(callback) {
+        this.onTodoListChanged = callback
+    }
+
+    _commit(toods) {
+        this.onTodoListChanged(toods)
+        localStorage.setItem('todos', JSON.stringify(toods))
+    }
+
+    addTodo(todoText) {
+        const todo = {
+            id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+            text: todoText,
+            complete: false,
+        }
+
+        this.todos.push(todo)
+
+        this._commit(this.todos)
+    }
+
+    editTodo(id, updatedText) {
+        this.todos = this.todos.map(todo =>
+            todo.id === id ? { id: todo.id, text: updatedText, complete: todo.complete } : todo
+        )
+
+        this._commit(this.todos)
+    }
+
+    deleteTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id)
+
+        this._commit(this.todos)
+    }
+
+    toggleTodo(id) {
+        this.todos = this.todos.map(todo =>
+            todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo
+        )
+
+        this._commit(this.todos)
+    }
+}
+
+export default Model;
